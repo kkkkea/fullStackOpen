048130326b0bd6136e2b02bcfe2a6a52534588f6
44787 <447872721@qq.com>
2023-05-22 20:13:47 +0800

    1

---
+++ part-1/intro_to_react/src/App.js
@@ -0,0 +1,75 @@
+const Header = (props) => {
+	return (
+		<>
+			<h1>{props.courseName}</h1>
+		</>
+	);
+};
+
+const Part = (props) => {
+	return (
+		<>
+			<p>
+				{props.part} {props.exercise}
+			</p>
+		</>
+	);
+};
+
+const Content = (props) => {
+	return (
+		<>
+			<Part
+				part={props.parts[0].name}
+				exercise={props.parts[0].exercise}
+			/>
+			<Part
+				part={props.parts[1].name}
+				exercise={props.parts[1].exercise}
+			/>
+			<Part
+				part={props.parts[2].name}
+				exercise={props.parts[2].exercise}
+			/>
+		</>
+	);
+};
+
+const Total = (props) => {
+	return (
+		<>
+			<p>
+				Number of exercise{" "}
+				{props.exercise.reduce((acc, v) => acc + v, 0)}
+			</p>
+		</>
+	);
+};
+
+const App = () => {
+	const course = "Half Stack application development";
+	const parts = [
+		{
+			name: "Fundamentals of react",
+			exercise: 10,
+		},
+		{
+			name: "Using props to pass data",
+			exercise: 7,
+		},
+		{
+			name: "State of a component",
+			exercise: 14,
+		},
+	];
+
+	return (
+		<>
+			<Header courseName={course} />
+			<Content parts={parts} />
+			<Total exercise={parts.map((p) => p.exercise)} />
+		</>
+	);
+};
+
+export default App;
